202a1407f1c19084d08aa550777d2609425c2754
shakiburrashid <shakiburrashid7@gmail.com>
2025-08-26 12:15:11 +0600

    all page crated also function created

--- script.js
+++ script.js
@@ -22,8 +22,79 @@ document.getElementById("submitButton").addEventListener("click",(e)=>{
     } else
     {
         alert('Error number or pin');
-        
     }
     
+})
+
+
+
+// ! Add Mouney Function
+document.getElementById('AddMoneyBtn').addEventListener("click",()=>{
+   
+    document.getElementById('addMoney').style.display='block';
+    document.getElementById('LatestPayment').style.display='none';
+    document.getElementById('cashoutMoney').style.display='none';
+    document.getElementById('transferMoney').style.display='none';
+    document.getElementById('getBonus').style.display='none';
+    document.getElementById('payBill').style.display='none';
+    document.getElementById('Transaction').style.display='none';
+
+})
+
+// ! Add Mouney Function
+document.getElementById('CashoutBtn').addEventListener("click",()=>{
+   
+    document.getElementById('addMoney').style.display='none';
+    document.getElementById('LatestPayment').style.display='none';
+    document.getElementById('cashoutMoney').style.display='block';
+    document.getElementById('transferMoney').style.display='none';
+    document.getElementById('getBonus').style.display='none';
+    document.getElementById('payBill').style.display='none';
+    document.getElementById('Transaction').style.display='none';
     
 })
+// ! Get Bonus Function
+document.getElementById('GetBonusBtn').addEventListener("click",()=>{
+   
+   document.getElementById('addMoney').style.display='none';
+    document.getElementById('LatestPayment').style.display='none';
+    document.getElementById('cashoutMoney').style.display='none';
+    document.getElementById('transferMoney').style.display='none';
+    document.getElementById('getBonus').style.display='block';
+    document.getElementById('payBill').style.display='none';
+    document.getElementById('Transaction').style.display='none';
+})
+
+// ! Paybill Function
+document.getElementById('PaybillBtn').addEventListener("click",()=>{
+   document.getElementById('addMoney').style.display='none';
+    document.getElementById('LatestPayment').style.display='none';
+    document.getElementById('cashoutMoney').style.display='none';
+    document.getElementById('transferMoney').style.display='none';
+    document.getElementById('getBonus').style.display='none';
+    document.getElementById('payBill').style.display='block';
+    document.getElementById('Transaction').style.display='none';
+})
+
+// ! Transfer Money Function
+document.getElementById('TransferMoneyBtn').addEventListener("click",()=>{
+   document.getElementById('addMoney').style.display='none';
+    document.getElementById('LatestPayment').style.display='none';
+    document.getElementById('cashoutMoney').style.display='none';
+    document.getElementById('transferMoney').style.display='block';
+    document.getElementById('getBonus').style.display='none';
+    document.getElementById('payBill').style.display='none';
+    document.getElementById('Transaction').style.display='none';
+})
+
+// ! Transacation Function
+document.getElementById('TransactionBtn').addEventListener("click",()=>{
+   document.getElementById('addMoney').style.display='none';
+    document.getElementById('LatestPayment').style.display='none';
+    document.getElementById('cashoutMoney').style.display='none';
+    document.getElementById('transferMoney').style.display='none';
+    document.getElementById('getBonus').style.display='none';
+    document.getElementById('payBill').style.display='none';
+    document.getElementById('Transaction').style.display='block';
+})
+
